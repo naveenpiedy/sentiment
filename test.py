85e8ee914ac1bd5282a35f3d34a75502519b56f1
Naveen Piedy <naveenpiedy@users.noreply.github.com>
2018-02-27 20:03:44 -0700

    Finished pickling and created modules.

---
+++ test.py
@@ -0,0 +1,4 @@
+import sentiment_mod as s
+
+print(s.sentiment("Waste of time. Wish I spent my tickets on something else."))
+print(s.sentiment("This movie was utter junk. There were absolutely 0 pythons. I don't see what the point was at all. Horrible movie, 0/10"))
